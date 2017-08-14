2e37240fb4cac98ecc32f945b1b80d30508cf5b2
kkh778 <goldrush778@yandex.com>
2017-08-14 12:11:14 +0800

    Css bug fixed

---
+++ install.sh
@@ -0,0 +1,4 @@
+echo 'remove old version'
+rm -rf ../../back_end_rails_json_api/vertex-asset-api/public/view/
+echo 'install new version'
+cp -rf ./dist ../../back_end_rails_json_api/vertex-asset-api/public/view/
